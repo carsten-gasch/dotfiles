a3abe90cc53e0bf9cee0ab6bdda53a8e44f6352d
Carsten Gasch <carsten.gasch@gmail.com>
2025-03-21 07:28:15 +0100

    add lsp-support and snacks.nvim for visuals

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -1,5 +1,7 @@
 vim.keymap.set("n", "-", "<cmd>Oil --float<CR>", {desc="Open Parent Directory"})
 
+vim.keymap.set("n", "gl", function() vim.diagnostic.open_float() end, {desc = "Open diagnostics in Float"})
+
 -- Navigate vim panes better
 vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
 vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')

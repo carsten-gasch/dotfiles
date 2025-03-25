206353a592be70deb1411e6d7dca74b2368ebed6
Carsten Gasch <carsten.gasch@gmail.com>
2025-03-25 14:14:34 +0100

    nvim: update plugins

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -4,14 +4,15 @@ vim.opt.softtabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.autoindent = true
 vim.opt.breakindent = true
+vim.opt.linebreak = true
 
-vim.opt.mouse='a'
+vim.opt.mouse = "a"
 
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
 vim.g.background = "light"
-vim.opt.signcolumn = 'yes'
+vim.opt.signcolumn = "yes"
 
 vim.opt.number = true
 vim.opt.relativenumber = true
@@ -22,14 +23,14 @@ vim.opt.showmode = false
 vim.opt.cmdheight = 0
 
 vim.schedule(function()
-  vim.opt.clipboard = 'unnamedplus'
+	vim.opt.clipboard = "unnamedplus"
 end)
 
 vim.opt.confirm = true
 vim.opt.scrolloff = 10
 
 vim.opt.list = true
-vim.opt.listchars = { tab = '» ', trail = '·', space = "·", nbsp = '␣', eol = "↲" }
+vim.opt.listchars = { tab = "» ", trail = "·", space = "·", nbsp = "␣", eol = "↲" }
 
 vim.opt.splitright = true
 vim.opt.splitbelow = true
@@ -40,14 +41,14 @@ vim.opt.timeoutlen = 300
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
 
-vim.opt.inccommand = 'split'
+vim.opt.inccommand = "split"
 
 vim.opt.swapfile = false
 
-vim.api.nvim_create_autocmd('TextYankPost', {
-  desc = 'Highlight when yanking (copying) text',
-  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
-  callback = function()
-    vim.highlight.on_yank()
-  end,
+vim.api.nvim_create_autocmd("TextYankPost", {
+	desc = "Highlight when yanking (copying) text",
+	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+	callback = function()
+		vim.highlight.on_yank()
+	end,
 })
